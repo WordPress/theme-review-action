ce7e83286cb9081eb4d80c9a442b71573b63e607
dufresnesteven <steve.dufresne@automattic.com>
2020-11-10 15:15:32 +0900

    Override the wp-env file to download gutenberg for block based themes.

--- index.js
+++ index.js
@@ -1,39 +1,75 @@
 const fs = require('fs');
 
+const THEME_PATH_ROOT = './test-theme';
+const READ_OPTIONS = { encoding: 'utf8' };
+
+const getParentTheme = () => {
+	try {
+		const styleLocation = `${THEME_PATH_ROOT}/style.css`;
+		const templateRegex = /Template:(\s?[^\s]+)/gim; // Template: ${parentTheme}
+
+		// Load in style.css to check for parent
+		const themeStyle = fs.readFileSync(styleLocation, READ_OPTIONS);
+		const template = themeStyle.match(templateRegex);
+		return template[0].toLowerCase().replace('template:', '').trim();
+	} catch (ex) {}
+
+	return false;
+};
+
+const isBlockBasedTheme = () => {
+	try {
+		fs.readFileSync(
+			`${THEME_PATH_ROOT}/block-templates/index.html`,
+			READ_OPTIONS
+		);
+
+		return true;
+	} catch (e) {}
+	return false;
+};
+
+const getEnvironmentConfig = () => {
+	try {
+		const jsonContent = fs.readFileSync('./.wp-env.json', READ_OPTIONS);
+		return JSON.parse(jsonContent);
+	} catch (e) {
+		console.log('Problem retrieving .wp-env', e);
+	}
+	return;
+};
+
 /**
  * Creates an override config file sometimes
  */
 const createOverrideConfig = () => {
-	const styleLocation = './test-theme/style.css';
-	const wpenvLocation = './.wp-env.json';
-	const wpenvOverrideLocation = './.wp-env.override.json';
-	const templateRegex = /Template:(\s?[^\s]+)/gim; // Template: ${parentTheme}
-	const readOptions = { encoding: 'utf8' };
+	const parentTheme = getParentTheme();
+	const isBlockBased = isBlockBasedTheme();
 
-	// Pull in style.css to check for parent
-	const themeStyle = fs.readFileSync(styleLocation, readOptions);
+	if (!parentTheme && !isBlockBased) {
+		console.log('No need for an override.');
+		return;
+	}
 
-	// Look for "Template: ..." in the style config.
-	const template = themeStyle.match(templateRegex);
+	const configs = getEnvironmentConfig();
 
-	// If its a child add the parent theme to the .wp-env.json and create override
-	if (template !== null) {
-		const jsonContent = fs.readFileSync(wpenvLocation, readOptions);
-		const parentTheme = template[0]
-			.toLowerCase()
-			.replace('template:', '')
-			.trim();
-		const configs = JSON.parse(jsonContent);
-
+	if (parentTheme) {
 		configs.themes.push(
 			`https://downloads.wordpress.org/theme/${parentTheme}.zip`
 		);
-		fs.writeFileSync(wpenvOverrideLocation, JSON.stringify(configs));
-
-		console.log('Created a .wp-env.override.json file.');
-	} else {
-		console.log('No need for an override.');
 	}
+
+	if (isBlockBased) {
+		configs.plugins.push(
+			`https://downloads.wordpress.org/plugin/gutenberg.zip`
+		);
+	}
+
+	fs.writeFileSync('./.wp-env.override.json', JSON.stringify(configs));
+
+	console.log('Created a .wp-env.override.json file.');
 };
 
 createOverrideConfig();
+
+//
