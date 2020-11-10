f9bd5e3ea5b0fe1b027065d89b9966492b8798fc
dufresnesteven <steve.dufresne@automattic.com>
2020-11-10 15:12:25 +0900

    Don't die when there isn't a menu.

--- bin/menu.sh
+++ bin/menu.sh
@@ -4,7 +4,6 @@
 MENU_ID=$( npm run wp-env run tests-cli "wp menu location list --format=csv | tail -n +2 | head -n 1 | cut -d, -f1" )
 CLEANED=$(echo $MENU_ID | grep -oE '[^ ]+$')
 
-npm run wp-env run tests-cli "wp menu location assign 'All Pages' $CLEANED"
- 
-
-
+if [ -z "$CLEANED" ]; then
+	npm run wp-env run tests-cli "wp menu location assign 'All Pages' $CLEANED"
+fi
