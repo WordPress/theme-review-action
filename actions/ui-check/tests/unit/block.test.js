9813ac66d01ff9fc49547d2823fb5318d15c3297
dufresnesteven <steve.dufresne@automattic.com>
2020-11-11 14:30:09 +0900

    Print properly.

--- actions/ui-check/tests/unit/block.test.js
+++ actions/ui-check/tests/unit/block.test.js
@@ -1,8 +1,13 @@
 /**
  * External dependencies
  */
-const fs = require( 'fs' );
+const fs = require('fs');
 import { parse } from '@wordpress/block-serialization-spec-parser';
+
+/**
+ * Internal dependencies
+ */
+import { printMessage } from '../e2e/utils';
 
 // Relative path
 const THEME_ROOT_FOLDER = '../../test-theme';
@@ -12,19 +17,19 @@ const THEME_ROOT_FOLDER = '../../test-theme';
  * @param {location} location Path to test theme folder
  * @returns {array}
  */
-const getFileContents = ( location ) => {
+const getFileContents = (location) => {
 	const arr = [];
 
-	fs.readdirSync( location ).forEach( ( file ) => {
-		const contents = fs.readFileSync( `${ location }/${ file }`, {
+	fs.readdirSync(location).forEach((file) => {
+		const contents = fs.readFileSync(`${location}/${file}`, {
 			encoding: 'utf8',
-		} );
+		});
 
-		arr.push( {
+		arr.push({
 			fileName: file,
 			contents: contents,
-		} );
-	} );
+		});
+	});
 
 	return arr;
 };
@@ -33,31 +38,32 @@ const getFileContents = ( location ) => {
  * Collects the template parts from theme
  */
 const getTemplates = () => {
-	const templateLocation = `${ THEME_ROOT_FOLDER }/block-templates`;
-	const templatePartLocation = `${ THEME_ROOT_FOLDER }/block-template-parts`;
+	const templateLocation = `${THEME_ROOT_FOLDER}/block-templates`;
+	const templatePartLocation = `${THEME_ROOT_FOLDER}/block-template-parts`;
 
 	return [
-		...getFileContents( templateLocation ),
-		...getFileContents( templatePartLocation ),
+		...getFileContents(templateLocation),
+		...getFileContents(templatePartLocation),
 	];
 };
 
-describe( 'Unit: Blocks', () => {
+describe('Unit: Blocks', () => {
 	// Load all the block templates to test their structure
 	let templates = getTemplates();
 
-	it( 'Should have properly formed gutenberg block comments in templates', async () => {
+	it('Should have properly formed gutenberg block comments in templates', async () => {
 		try {
-			for ( let i = 0; i < templates.length; i++ ) {
-				const [ block ] = parse( templates[ i ].contents );
+			for (let i = 0; i < templates.length; i++) {
+				const [block] = parse(templates[i].contents);
 
-				// When there is a problem parsing due to improper formatting, the blockName will return null
-				if ( block.blockName === null ) {
+				if (block.blockName === null) {
 					throw Error(
-						`There's a problem with ${ templates[ i ].fileName }. \n\n ${ templates[ i ].contents }`
+						`There's a problem with ${templates[i].fileName}. \n\n ${templates[i].contents}`
 					);
 				}
 			}
-		} catch ( ex ) {}
-	} );
-} );
+		} catch (ex) {
+			printMessage('warning', ['[ Block Tests ]', ex.message]);
+		}
+	});
+});
