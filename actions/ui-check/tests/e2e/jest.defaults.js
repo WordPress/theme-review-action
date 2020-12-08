da6ed84a5f61ed2f4a6cd821ab49bf13506e6474
dufresnesteven <steve.dufresne@automattic.com>
2020-12-08 15:22:16 +0900

    Create screenshot folder programmatically.

--- actions/ui-check/tests/e2e/jest.defaults.js
+++ actions/ui-check/tests/e2e/jest.defaults.js
@@ -1,8 +1,18 @@
+const fs = require( 'fs' );
+
 /**
  * Runs before all tests
  */
 
+const createScreenshotFolder = ( dir ) => {
+	if ( ! fs.existsSync( dir ) ) {
+		fs.mkdirSync( dir );
+	}
+};
+
 beforeAll( async () => {
+	createScreenshotFolder( '../../screenshots' );
+
 	// We are currently only applying tests for desktop sizing
 	await page.setViewport( { width: 1280, height: 800 } );
 } );
