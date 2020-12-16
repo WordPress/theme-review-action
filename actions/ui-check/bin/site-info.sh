a755d21243f6cd6c649909d3c32da9aa140f414b
dufresnesteven <steve.dufresne@automattic.com>
2020-12-16 10:30:38 +0900

    Set the default port in case its run locally.

--- actions/ui-check/bin/site-info.sh
+++ actions/ui-check/bin/site-info.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl "http://localhost:$WP_ENV_TESTS_PORT/?rest_route=/theme-test-helper/v1/info" > tests/e2e/specs/page/siteinfo.json
+# Set a default in case it's ran locally
+TEST_PORT=${WP_ENV_TESTS_PORT:=8889}
+
+curl "http://localhost:$TEST_PORT/?rest_route=/theme-test-helper/v1/info" > tests/e2e/specs/page/siteinfo.json
