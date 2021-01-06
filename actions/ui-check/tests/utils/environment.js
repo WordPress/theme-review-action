78335a9207ec02821328ce4b517d1bd80faead76
dufresnesteven <steve.dufresne@automattic.com>
2021-01-06 17:26:33 +0900

    Check ou the env.

--- actions/ui-check/tests/utils/environment.js
+++ actions/ui-check/tests/utils/environment.js
@@ -1,6 +1,8 @@
 export const isDebugMode = () => process.env.UI_DEBUG;
 export const testPort = process.env.WP_ENV_TESTS_PORT || 8889;
 
+console.log( process.env );
+
 export const getEnvironmentVariable = ( variable ) => {
 	return variable || false;
 };
