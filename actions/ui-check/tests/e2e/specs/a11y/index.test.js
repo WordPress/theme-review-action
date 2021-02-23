9e7ed83606e9c1a4f95d00eeeb173ce0543c358e
Steven Dufresne <steve.dufresne@automattic.com>
2021-02-23 13:25:28 +0900

    Update a11y spec. Setting the wrong log.

--- actions/ui-check/tests/e2e/specs/a11y/index.test.js
+++ actions/ui-check/tests/e2e/specs/a11y/index.test.js
@@ -14,7 +14,7 @@ describe( 'Accessibility', () => {
 	const envVar = getEnvironmentVariable( process.env.TEST_ACCESSIBILITY );
 	const testAccessibility = envVar === 'true';
 	const accessibilityTest = testAccessibility ? 'wcag2a' : 'best-practice';
-	const noticeType = testAccessibility ? 'setFailed' : 'warnings';
+	const noticeType = testAccessibility ? 'errors' : 'warnings';
 
 	test.each( urls )(
 		`Should pass ${accessibilityTest} Axe tests on %s`,
