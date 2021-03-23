b4c71653f7e7bd2bc8508a2857e56ee8cb9f799a
dufresnesteven <steve.dufresne@automattic.com>
2021-03-23 13:56:21 +0900

    Refactor ui tests to only visit once.

--- actions/ui-check/tests/e2e/specs/ui/index.test.js
+++ actions/ui-check/tests/e2e/specs/ui/index.test.js
@@ -9,23 +9,23 @@ import elementFocusTest from './element-focus';
 import tabbingTest from './tabbing';
 
 describe( 'Accessibility: UI', () => {
-	it( 'Should have skip links', async () => {
+	beforeAll( async () => {
 		await page.goto( createURL( '/' ) );
+	} );
+
+	it( 'Should have skip links', async () => {
 		await skipLinksTest();
 	} );
 
 	it( 'Should have appropriate submenus', async () => {
-		await page.goto( createURL( '/' ) );
 		await subMenuTest();
 	} );
 
 	it( 'Should have element focus state', async () => {
-		await page.goto( createURL( '/' ) );
 		await elementFocusTest();
 	} );
 
 	it( 'Should have logical tabbing', async () => {
-		await page.goto( createURL( '/' ) );
 		await tabbingTest();
 	} );
 } );
