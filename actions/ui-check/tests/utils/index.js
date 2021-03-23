10d89d94d31ba4696568932ded019e63744931d3
dufresnesteven <steve.dufresne@automattic.com>
2021-03-23 14:09:27 +0900

    Remove unused network file.

--- actions/ui-check/tests/utils/index.js
+++ actions/ui-check/tests/utils/index.js
@@ -1,7 +1,6 @@
 export * from './environment';
 export * from './formatting';
 export * from './messaging';
-export * from './network';
 export * from './queries';
 export * from './screenshots';
 export * from './settings';
