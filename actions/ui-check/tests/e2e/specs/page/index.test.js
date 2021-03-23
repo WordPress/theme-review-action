669ee9590b192be46be9ab48abbef81871de3edb
dufresnesteven <steve.dufresne@automattic.com>
2021-03-23 13:47:59 +0900

    Speed up the page tests by not revisiting the same page in each test 12s -> 2s.

--- actions/ui-check/tests/e2e/specs/page/index.test.js
+++ actions/ui-check/tests/e2e/specs/page/index.test.js
@@ -8,7 +8,6 @@ const fetch = require( 'node-fetch' );
  */
 import { createURL, getTestUrls } from '../../../utils';
 
-
 import bodyClassTest from './body-class';
 import phpErrorsTest from './php-errors';
 import completeOutputTest from './complete-output';
@@ -22,49 +21,40 @@ let urls = [ [ '/', '?feed=rss2', '' ], ...getTestUrls() ];
 
 // Some basic tests that apply to every page
 describe.each( urls )( 'Test URL %s%s', ( url, queryString, bodyClass ) => {
+	let pageResponse, fullUrl;
+
+	beforeAll( async () => {
+		fullUrl = createURL( url, queryString );
+		pageResponse = await page.goto( fullUrl );
+	} );
+
 	it( 'Page should contain body class ' + bodyClass, async () => {
 		// Make sure the page content appears to be appropriate for the URL.
-		const fullUrl = createURL( url, queryString );
-		await page.goto( fullUrl );
-
 		await bodyClassTest( fullUrl, bodyClass );
 	} );
 
 	it( 'Page should not have PHP errors', async () => {
-		await page.goto( createURL( url, queryString ) );
-
 		await phpErrorsTest();
 	} );
 
 	it( 'Page should have complete output', async () => {
 		// This should catch anything that kills output before the end of the page, or outputs trailing garbage.
-		const fullUrl = createURL( url, queryString );
-		const response = await page.goto( fullUrl );
-		const text = await response.text();
-
+		const text = await pageResponse.text();
 		await completeOutputTest( fullUrl, text );
 	} );
 
 	it( 'Page should return 200 status', async () => {
-		const fullUrl = createURL( url, queryString );
-		let response = await page.goto( fullUrl );
-		const status = await response.status();
+		const status = await pageResponse.status();
 
 		await pageStatusTest( fullUrl, status );
 	} );
 
 	it( 'Browser console should not contain errors', async () => {
-		const fullUrl = createURL( '/' );
-
-		await page.goto( fullUrl );
 		await jsErrorTest( fullUrl );
 	} );
 
 	it( 'Page should not have unexpected links', async () => {
 		// See https://make.wordpress.org/themes/handbook/review/required/#selling-credits-and-links
-		const fullUrl = createURL( url, queryString );
-		await page.goto( fullUrl );
-
 		await unexpectedLinksTest( fullUrl, queryString );
 	} );
 } );
