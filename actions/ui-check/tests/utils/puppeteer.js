57d67edca832205617452f3601c8ffabd5e1c0fc
dufresnesteven <steve.dufresne@automattic.com>
2021-03-24 09:31:24 +0900

    Clean up query string.

--- actions/ui-check/tests/utils/puppeteer.js
+++ actions/ui-check/tests/utils/puppeteer.js
@@ -3,6 +3,10 @@ const fs = require( 'fs' );
 import { createURL } from './environment';
 import { OUTPUT_HTML_FOLDER_PATH } from './paths';
 
+const cleanQueryStringForFileSystem = ( queryString ) => {
+	return queryString.replace( '?', '-' );
+};
+
 export const goTo = async ( url, queryString = '' ) => {
 	let response = await page.goto( createURL( url, queryString ) );
 	let content = await response.text();
@@ -10,7 +14,8 @@ export const goTo = async ( url, queryString = '' ) => {
 	try {
 		let path = [
 			OUTPUT_HTML_FOLDER_PATH,
-			url.replace( '/', 'home' ) + queryString,
+			url.replace( '/', 'home' ) +
+				cleanQueryStringForFileSystem( queryString ),
 		];
 
 		fs.writeFileSync( `${ path.join( '/' ) }.html`, content );
